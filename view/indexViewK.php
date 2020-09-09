b88fa1612ffa067e1794c73419938d7f59fa70ad
Kevin 'Sumpheus' Nguma <51791578+Sumpheus@users.noreply.github.com>
2020-09-09 13:38:21 +0200

    Bah c'est fait !

--- view/indexViewK.php
+++ view/indexViewK.php
@@ -16,23 +16,23 @@ foreach ($visuals as $visual) {}
       <ul>
         <li><p>Nom : <input type="text" name="nom" value="<?php echo $visual['nom']?>"/></p></li>
 
-        <li><p>Date d'achat : <input type="date" name="date_achat" value=""/></p></li>
+        <li><p>Date d'achat : <input type="date" name="date_achat" value="<?php echo $visual['date_achat']?>"/></p></li>
 
-        <li><p>Date de garantie : <input type="date" name="date_garantie" value=""/></p></li>
+        <li><p>Date de garantie : <input type="date" name="date_garantie" value="<?php echo $visual['date_garantie']?>"/></p></li>
 
-        <li><p>Prix : <input type="text" name="prix" value=""/></p></li>
+        <li><p>Prix : <input type="text" name="prix" value="<?php echo $visual['prix']?>"/></p></li>
 
-        <li><p>Conseil : <input type="text" name="conseil" value=""/></p></li>
+        <li><p>Conseil : <input type="text" name="conseil" value="<?php echo $visual['conseil']?>"/></p></li>
 
-        <li><p>Photo Ticket : <input type="file" accept="image/jpeg, image/png, image/jpg" name="photo_ticket" value=""/></p></li>
+        <li><p>Photo Ticket : <input type="file" accept="image/jpeg, image/png, image/jpg" name="photo_ticket" value="<?php echo $visual['photo_ticket']?>"/></p></li>
 
-        <li><p>Photo : <input type="file" accept="image/jpeg, image/png, image/jpg" name="photo" value=""/><p></li>
+        <li><p>Photo : <input type="file" accept="image/jpeg, image/png, image/jpg" name="photo" value="<?php echo $visual['photo']?>"/><p></li>
 
-        <li><p>Catégorie : <input type='text' name="categorie" value=""/></p></li>
+        <li><p>Catégorie : <input type='text' name="categorie" value="<?php echo $visual ['categorie']?>"/></p></li>
 
-        <li><p>référence : <input type='text' name="reference" value=""/></p></li>
+        <li><p>référence : <input type='text' name="reference" value="<?php echo $visual ['reference']?>"/></p></li>
 
-        <li><p>id : <input type='number' name="id" value=""/></p></li>
+        <li><p>id : <input type='number' name="id" value="<?php echo $visual ['id']?>"/></p></li>
 
         <li><p><input type="submit" name="edit" value="Edit"/><p><li>
       </ul>
@@ -41,11 +41,9 @@ foreach ($visuals as $visual) {}
 <!-- displaying images from the table -->
     <div>
       <div id=images>
-
-        <img src=" <?php ?> ">;
-
+        <img src=" <?php echo $visual ['photo_ticket'] ?> ">;
       </div>
-      <?php ?>
+        <img src=" <?php echo $visual ['photo']?> ">"
     </div>
 
   </body>
