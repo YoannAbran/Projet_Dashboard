aa47367989e54e00ab71df0b20937e736df8fea2
Belios <64775563+Belios@users.noreply.github.com>
2020-09-15 16:49:55 +0200

    Dash Front

--- view/DashView.php
+++ view/DashView.php
@@ -2,7 +2,7 @@
   ob_start();
   session_start();
   session_regenerate_id();
-<<<<<<< HEAD
+
 ?>
 
     <link rel="stylesheet" href="main.css">
@@ -57,13 +57,9 @@
           <!-- Plugins searchbar/notifications/mails/login admin/  -->
         </div>
 
-        <div class="dash-title">
-          <h1> Page d'accueil / Dashboard </h1>
-          <h6 id="subtitle"> Voici la description de la page d'accueil du dashboard </h6>
-        </div>
 
 
-        <div class="card-deck">
+        <!-- <div class="card-deck">
           <div class="card">
             <div class="card-body">
               <img src="../template/img/money.png">
@@ -92,14 +88,14 @@
             </div>
             <img src="../template/img/testgraph.jpg" class="card-img-top" alt="...">
           </div>
-        </div>
+        </div> -->
 
 
-          <div class="card-deck">
+          <!-- <div class="card-deck">
             <div class="card cardlist">
-              <div class="card-body">
+              <div class="card-body"> -->
                 <!-- <img src="../template/img/crown.png" alt="..."> -->
-                <table class="table">
+                <!-- <table class="table">
                   <div class="title">
                   <img id="crown" src="../template/img/crown1.png">
                   <h5 class="card-title titlelist">Best-Sellers</h5>
@@ -170,8 +166,8 @@
                   </tbody>
                 </table>
               </div>
-            </div>
-            <div class="card">
+            </div> -->
+            <!-- <div class="card">
               <div class="card-body" style="border-radius: 0%;">
                 <h5 class="card-title">Stocks</h5>
               </div>
@@ -180,16 +176,13 @@
           </div>
         </div>
       </div>
+    </div> -->
+
+
+    <div class="dash-title">
+      <h1> Dashboard </h1>
+      <h6 id="subtitle"> Voici la description de la page d'accueil du dashboard </h6>
     </div>
-
-    <?php
-      $content = ob_get_clean();
-      require('template.php');
-=======
-
-?>
-
-
 
       <!-- Content -->
       <div class="container-fluid d-flex justify-content-center row" id="content">
@@ -200,25 +193,26 @@
 
         <div id="graph" class="container-fluid d-flex flex-column align-items-center row p-2 col-md-9">
           <div class="container d-flex justify-content-center col row p-2 ">
-          <div class="container border border-dark col-lg-6 p-2">
+          <div id="shadow" class="container border border-dark col-lg-6 p-2">
+            <div class="categorie"><h4>Ventes par catégories global</h4></div>
             <canvas id="myChart" ></canvas>
           </div>
 
-          <div class="container border border-dark col-lg-6 p-2" >
+          <div id="shadow" class="container border border-dark col-lg-6 p-2" >
             <canvas class="p-2" id="myChart2" ></canvas>
           </div>
         </div>
           <div class="container d-flex justify-content-center col row  p-2">
-          <div class="container border border-dark col-lg-6 p-2">
+          <div id="shadow" class="container border border-dark col-lg-6 p-2">
             <canvas class="p-2" id="myChart3" ></canvas>
           </div>
-          <div class="container border border-dark col-lg-6 p-2">
+          <div id="shadow" class="container border border-dark col-lg-6 p-2">
             <canvas class="p-2" id="myChart4" ></canvas>
         </div>
       </div>
           </div>
 
-          <div class="container col-md-3" id="list_book">
+          <div class="container col-md-3 shadowlist" id="list_book">
 
             <div class="container d-flex flex-column align-items-center justify-content-center">
                   <img id='king' src="template/img/crown.png" alt="...">
@@ -239,6 +233,8 @@
                       </tr>
                     </thead>
                     <tbody>
+
+
                       <?php
                       $count = 0;
                       foreach ($ventetop as $top) {
@@ -263,30 +259,76 @@
                       }
 
                        ?>
-
                     </tbody>
                   </table>
-
-                  <div class="simple_data  m-2 p-2 text-center  rounded-pill ">
-                    <p>Total des ventes : <span class="font-weight-bolder"><?php echo prixtotalvente();?></span> €<br></p>
-                    </div>
-                  <div class="simple_data  m-2 p-2 text-center rounded-pill">
-                      <p>Total des achat : <span class="font-weight-bolder"><?php echo testctotgraph();?></span> €<br></p>
-                    </div>
-                  <div class="simple_data  m-2 p-2 text-center rounded-pill">
-                    <p>Nombre d'achat : <span class="font-weight-bolder"><?php echo nbretotalachat();?></span><br></p>
-                    </div>
-                  <div class="simple_data  m-2 p-2 text-center rounded-pill">
-                    <p>Nombre de vente : <span class="font-weight-bolder"><?php echo nbretotalvente();?></span><br></p>
-                    </div>
-                  </div>
-
-
                 </div>
-        </div>
+              </div>
+
+          <div class="row mt-5 scoreboard container-fluid">
+            <!-- <div class="card bg-dark text-white vw-100"> -->
+              <!-- <div class="card-img-overlay"> -->
+                <div class="row text-center container-fluid vw-100">
+                  <div id="text-appear">
+                    <ul class="text hidden d-flex">
+                      <li class="ghost">S</li>
+                      <li class="ghost">C</li>
+                      <li class="ghost">O</li>
+                      <li class="ghost">R</li>
+                      <li class="ghost">E</li>
+                      <li class="ghost">B</li>
+                      <li class="ghost">O</li>
+                      <li class="ghost">A</li>
+                      <li class="ghost">R</li>
+                      <li class="ghost">D</li>
+                    </ul>
+                  </div>
+                  <div class="col">
+                    <div class="counter">
+                      <i class="fa fa-line-chart fasize"></i>
+                      <h2 class="timer count-title count-number" data-to="<?php echo prixtotalvente();?>" data-speed="2500"></h2>
+                      <p class="count-text ">Montant des ventes/€</p>
+                    </div>
+                    </div>
+                      <div class="col">
+                        <div class="counter">
+                          <i class="fa fa-shopping-basket fasize"></i>
+                          <h2 class="timer count-title count-number" data-to="<?php echo testctotgraph();?>" data-speed="2500"></h2>
+                          <p class="count-text ">Montant des achats/€</p>
+                        </div>
+                      </div>
+                      <div class="col">
+                        <div class="counter">
+                          <i class="fa fa-truck fasize"></i>
+                          <h2 class="timer count-title count-number" data-to="<?php echo nbretotalachat();?>" data-speed="2500"></h2>
+                          <p class="count-text ">Stock: </p>
+                        </div>
+                      </div>
+                        <div class="col">
+                          <div class="counter">
+                            <i class="fa fa-handshake-o fasize"></i>
+                            <h2 class="timer count-title count-number" data-to="<?php echo    nbretotalvente();?>" data-speed="2500"></h2>
+                            <p class="count-text ">Total des ventes</p>
+                          </div>
+                        </div>
+                      </div>
+                    <!-- </div> -->
+                  <!-- </div> -->
+                </div>
 
 
-
+        <!-- <div class="simple_data  m-2 p-2 text-center  rounded-pill ">
+          <p>Total des ventes : <span class="font-weight-bolder"><?php echo prixtotalvente();?></span> €<br></p>
+          </div>
+        <div class="simple_data  m-2 p-2 text-center rounded-pill">
+            <p>Total des achat : <span class="font-weight-bolder"><?php echo testctotgraph();?></span> €<br></p>
+          </div>
+        <div class="simple_data  m-2 p-2 text-center rounded-pill">
+          <p>Nombre d'achat : <span class="font-weight-bolder"><?php echo nbretotalachat();?></span><br></p>
+          </div>
+        <div class="simple_data  m-2 p-2 text-center rounded-pill">
+          <p>Nombre de vente : <span class="font-weight-bolder"><?php echo nbretotalvente();?></span><br></p>
+          </div>
+        </div> -->
 
 
 
@@ -446,7 +488,9 @@
         return pool;
     }
     </script>
+
+
+
 <?php
 $content = ob_get_clean();
  require('template.php');
->>>>>>> ea90533c5376cedbadadcf487d96f5eb447a9414
--- view/template.php
+++ view/template.php
@@ -21,8 +21,8 @@ $title = "dashboard";
       echo "<p class='alert alert-secondary text-center p-0 m-0'>Welcome " . $_SESSION['authUser']. "</p>";
 
   }?>
-  <nav id="navstyle" class="navbar navbar-expand-lg navbar-dark">
-    <a class="navbar-brand" href="#">Navbar</a>
+  <nav id="navstyle" class="navbar navbar-expand-lg navbar-light">
+    <a class="navbar-brand" href="view/DashView.php"><img src="./template/img/logo.png"></a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
@@ -68,8 +68,9 @@ if ( confirm("Etes vous sur de vouloir effacer ce fichier?") == false ) {
 }
 }
 </script>
-  <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
-  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+<script src="public/script/script.js"></script>
 </body>
 </html>
