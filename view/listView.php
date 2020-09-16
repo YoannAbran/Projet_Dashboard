38015c1d5b91c7d9279172444c82d0a8cf5e38b4
Yoann Abran <y.abran@codeur.online>
2020-09-16 08:41:54 +0200

    master

--- view/listView.php
+++ view/listView.php
@@ -3,7 +3,18 @@
   session_start();
   session_regenerate_id();
 ?>
+<div class="">
+  <h1>Home</h1>
+</div>
+  <table class="table table-bordered table-dark table-sm">
+<tbody>
+  <?php  foreach ($rows as $row){
+  echo "<tr><td class='px-5'><a class='text-light' href='index.php?action=edit&id=".$row['id']."'>".$row['nom']."</a></td>";
+  echo "<td>".$row['reference']."</td>";
+    if (isset($_SESSION['isAdmin'])) {
+  echo"<td><form action ='index.php?action=delete&idel=".$row['id']."'' method='post' onsubmit='return submitResult();'><input type='submit' name='suppr' value='Supprimer'></form></td></tr>";
 
+}
 <table class='table table-striped table-dark'>
           <thead>
             <tr>
@@ -24,8 +35,13 @@
           </thead>
         <tbody>
 
+    } ?>
 
+</tbody>
+      </table>
 <?php
+$content = ob_get_clean();
+ require('template.php');
     foreach ($books as $book) {
 ?>
       <tr>
